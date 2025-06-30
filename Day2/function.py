0878d6902fa049bade9c159cb19e8548d166e751
Poovarasan E <epoovarasansujith25@gmail.com>
2025-06-30 20:46:00 +0530

    added Day2 on python learning

---
+++ Day2/function.py
@@ -0,0 +1,11 @@
+a = 20
+b = 30
+def add(a, b):
+   return a + b
+result1=add(20, 30)
+print("Addition_of_A_and_B:", result1)
+
+def sub(a, b):
+   return a - b
+result2=sub(20, 30)
+print("Subtraction_of_A_and_B:" , result2)
---
+++ Day2/module.py
@@ -0,0 +1,4 @@
+import function as new
+
+total = new.add(20, 12)
+print("Addition:" ,total)
